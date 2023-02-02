3409e61867feda2a891a649aa5600dcde5bbb372
Chrisdu44 <christopherguichard44@gmail.com>
2023-02-02 16:55:00 +0100

    mise à jour des données + ajout de la db

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,61 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE DATABASE IF NOT EXISTS portfoliochristopher;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+USE portfoliochristopher;
+
+DROP TABLE IF EXISTS `user`;
+
+DROP TABLE IF EXISTS technology;
+
+DROP TABLE IF EXISTS `project`;
+
+CREATE TABLE
+    `user` (
+        id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE
+    );
+
+INSERT INTO
+    `user` (
+        first_name,
+        last_name,
+        `password`,
+        email
+    )
+VALUES (
+        "Christopher",
+        "Guichard",
+        "666Diablo13",
+        "christopherguichard44@gmail.com"
+    );
+
+CREATE TABLE
+    technology (
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        name VARCHAR(500) NOT NULL
+    ) ENGINE = InnoDB;
+
+INSERT INTO technology (name)
+VALUES ("JavaScript"), ("React"), ("CSS3"), ("HTML5"), ("MySQL");
+
+CREATE TABLE
+    `project` (
+        id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        title VARCHAR(150) NOT NULL,
+        description VARCHAR(200) NOT NULL,
+        picture_url VARCHAR(1000) NOT NULL,
+        github_url VARCHAR(1000) NULL,
+        site_url VARCHAR(1000) NULL
+    );
+
+CREATE TABLE
+    `project_has_technology` (
+        project_id INT NOT NULL,
+        technology_id INT NOT NULL
+    );
+
+INSERT INTO
+    `project_has_technology` (project_id, technology_id)
+VALUES (1, 1);
